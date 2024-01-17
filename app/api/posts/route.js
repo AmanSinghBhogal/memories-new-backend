b5ee4f953ad884bb811c434eefb494cfd8acd86b
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2024-01-17 17:33:34 +0530

    Posts Get request configured

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -4,13 +4,24 @@ import PostMessage from "@models/postMessage";
 export const dynamic = "force-dynamic";
 export const revalidate = 1; //revalidate api every 1 second
 
-export const GET = async (request) => {
+export const GET = async (req) => {
+
+    const page = parseInt(req.nextUrl.searchParams.get("page"));
+    const limit = req.nextUrl.searchParams.get("limit");
+    
     try {
         await connectToDB();
+        const defaultPageLimit = 3;
+        const Limit = (Number(limit)? Number(limit): defaultPageLimit);
+        const Page = (Number(page)>0? Number(page) : 1);
+        const StartIndex = (Page - 1) * Limit;
+        const TotalPosts = await PostMessage.countDocuments({});
+        const posts = await PostMessage.find().sort({ _id: -1 }).limit(Limit).skip(StartIndex);
 
-        const posts = await PostMessage.find();
+        return new Response(JSON.stringify({ data: posts, currentPage: Page, numberOfPages: Math.ceil(TotalPosts/Limit) }), { status: 200 });
 
-        return new Response(JSON.stringify(posts), { status: 200 });
+        // const posts = await PostMessage.find();
+        // return new Response(JSON.stringify(posts), {status: 200});
         
     } catch (error) {
         
