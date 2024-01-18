3b49003299f7152f498117c0c62ab5625f08a812
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2024-01-18 15:53:49 +0530

    Delete Endpoint added to posts route

--- app/api/posts/[id]/route.js
+++ app/api/posts/[id]/route.js
@@ -1,14 +1,32 @@
 import { connectToDB } from "@utils/database";
 import PostMessage from "@models/postMessage";
 import mongoose from "mongoose";
+import jwt from 'jsonwebtoken';
 
 export const revalidate = 1; //revalidate api every 1 second
 
 // Patch Endpoint
 export const PATCH = async (req, { params }) => {
-    const id = params.id;
-    const post = req.body;
     try {
+        const id = params.id;
+        const post = req.body;
+
+        // Checking if the request is authenticated or not:
+        const token = req.headers.authorization.split(" ")[1];
+        const isCustom = token.length < 500;
+        let decodedData;
+
+        if(token && isCustom)
+        {
+            decodedData = jwt.decode(token, process.env.JWT_SECRET);
+            req.userId = decodedData?.id;
+        }
+        else
+        {
+            decodedData = jwt.decode(token);
+            req.userId = decodedData?.sub;
+        }
+
         await connectToDB();
         if(!mongoose.Types.ObjectId.isValid(id)){
             return new Response(`No post with id: ${id}`, { status: 404 });
@@ -23,5 +41,39 @@ export const PATCH = async (req, { params }) => {
     } catch (error) {
         console.log(error);
         return new Response("Failed to update Post.", { status: 500 });
+    }
+}
+
+// Delete Endpoint
+export const DELETE = async (req, { params }) => {
+    try {
+        // Checking if the request is authenticated or not:
+        const token = req.headers.authorization.split(" ")[1];
+        const isCustom = token.length < 500;
+        let decodedData;
+
+        if(token && isCustom)
+        {
+            decodedData = jwt.decode(token, process.env.JWT_SECRET);
+            req.userId = decodedData?.id;
+        }
+        else
+        {
+            decodedData = jwt.decode(token);
+            req.userId = decodedData?.sub;
+        }
+
+        await connectToDB();
+
+        if(!mongoose.Types.ObjectId.isValid(id))
+            return new Response(`No post with id: ${id}`, { status: 404 });
+        
+        await PostMessage.findByIdAndDelete(params.id);
+
+        return new Response("Post Deleted Successfully", { status: 200 });
+        
+    } catch (error) {
+        console.log(error);
+        return new Response("Failed to Delete Post!", { status: 500 });
     }
 }
